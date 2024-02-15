433a7c9f00056ac1819d99a4325970e434a94d11
nslllll <tim041126@hotmail.com>
2024-02-15 23:01:28 +0800

    Initial Commit

---
+++ ARC1/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  ARC1
+//
+//  Created by Ng Sui Lam on 29/1/2024.
+//
+
+import SwiftUI
+import RealityKit
+
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+struct ARViewContainer:  UIViewRepresentable{
+    func makeUIView(context: Context) -> some UIView {
+        let arView = ARView(frame: .zero)
+        
+        return arView
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
+
+
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
